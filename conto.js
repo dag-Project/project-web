075aeb198140b265dfc9d60621cf2aaa29767772
dag-Project <91671287+dag-Project@users.noreply.github.com>
2021-09-30 19:02:27 +0700

    Update conto.js

--- conto.js
+++ conto.js
@@ -1,3 +1,9 @@
 function myFunction(){
+    var nameValue = document.login.username.value;
+      var password =  document.login.password.value;
+    alert(nameValue+'|'+password)
+}
+
+function myFunct(){
     alert('Selamat Datang')
 }
